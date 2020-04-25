f557c01673a785c84a3f9257256e41bfd203eac5
Marcus Lau <lau.marcus76@gmail.com>
2020-04-25 00:58:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+  
+
+end
+
+  fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+# Creates "local" variables is outside of the class definition. 
+# More like creating local instances of class Dog? 
